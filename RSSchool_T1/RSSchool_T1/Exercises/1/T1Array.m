072e86de502df350711f781890aa72c7289f978d
Siarhei Burakouski <serge.burakovsky@gmail.com>
2020-03-12 00:01:09 +0300

    feat: add exercise 1

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,30 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *result;
+    NSArray *res;
+    result = [NSMutableArray array];
+    res = [NSArray array];
+
+    NSUInteger cnt;
+    cnt = [sadArray count];
+    if (cnt <= 2) return sadArray;
+    
+    [result addObject: [sadArray firstObject]];
+    int i;
+    for (i = 1; i < cnt-1; i++) {
+        id prevElement = [sadArray objectAtIndex:i-1];
+        id currElement = [sadArray objectAtIndex:i];
+        id nextElement = [sadArray objectAtIndex:i+1];
+        if (([prevElement intValue] + [nextElement intValue]) > [currElement intValue]){
+            [result addObject:currElement];
+        }
+    }
+    [result addObject: [sadArray lastObject]];
+
+    bool flag = [result isEqualToArray:sadArray];
+    res = (flag)? result : [self convertToHappy:result];
+    return res;
 }
 
 @end
