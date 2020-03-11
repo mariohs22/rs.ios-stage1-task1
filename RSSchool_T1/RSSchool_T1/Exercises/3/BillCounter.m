a175c1e9f94a1587cf05c9119369772f262be317
Siarhei Burakouski <serge.burakovsky@gmail.com>
2020-03-12 01:52:26 +0300

    feat: add exercise 3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,18 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    NSMutableArray *tmpBill;
+    
+    tmpBill = [NSMutableArray array];
+    [tmpBill addObjectsFromArray:bill];
+    [tmpBill removeObjectAtIndex:index];
+    NSNumber *partAnna = [tmpBill valueForKeyPath:@"@sum.self"];
+    NSUInteger dpartAnna = [partAnna intValue] / 2;
+    NSUInteger toAnna = [sum intValue] - dpartAnna;
+    
+    NSString *result = ([sum intValue] == dpartAnna)? @"Bon Appetit" :
+        (toAnna > 0)? [NSString stringWithFormat: @"%lu", toAnna] : @"Bon Appetit";
+    return result;
 }
 
 @end
